9b8dbc9b3a069c2e632ed74cc105f2c2f64c8173
adrusi <adrian@adrusi.com>
2015-04-11 03:58:56 -0400

    Working random numbers vv XMLHttpRequest from server.adrusi

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,25 @@
 Pebble.addEventListener("ready",
-    function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
-    }
+  function(e) {
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://maps.googleapis.com/maps/api/directions/json?origin=here&destination=NS%20Norfolk&mode=walking&avoid=highways&arrival_time='+(Date.now()+600000)+'&key=AIzaSyCxTPa24kl86coKhxb_h7xdyLgq9dCKmuw');
+    req.onload = function(e) {
+    // console.log('ready');
+    var i = 0;
+    Pebble.sendAppMessage({0: ""+i}, function resend() {
+      // console.log('XMLHttpRequest()');
+      req.open('GET', 'http://server.adrusi.com:25565', true);
+      req.onload = function(e) {
+        // console.log('req.onload');
+        if (req.readyState == 4 && req.status == 200) {
+          if(req.status == 200) {
+            // console.log(req.responseText);
+            Pebble.sendAppMessage({0: ""+req.responseText}, setTimeout(resend, 1000), null);
+          } else { console.log('Error'); }
+        }
+      };
+      req.send(null);
+    }, null);
+    };
+    req.send(null);
+  }
 );
--- src/tbt.c
+++ src/tbt.c
@@ -2,6 +2,42 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static TextLayer *overlay_layer;
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Get the first pair
+  Tuple *t = dict_read_first(iterator);
+
+  // Process all pairs present
+  while (t != NULL) {
+    // Long lived buffer
+    static char s_buffer[64];
+
+    // Process this pair's key
+    switch (t->key) {
+      case 0:
+        // Copy value and display
+        snprintf(s_buffer, sizeof(s_buffer), "%s", t->value->cstring);
+        text_layer_set_text(overlay_layer, s_buffer);
+        break;
+    }
+
+    // Get next pair, if any
+    t = dict_read_next(iterator);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
@@ -26,9 +62,13 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text(text_layer, "Press ¤ button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+
+  overlay_layer = text_layer_create((GRect) { .origin = { 0, 36 }, .size = { bounds.size.w, 20 } });
+
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  layer_add_child(window_layer, text_layer_get_layer(overlay_layer));
 }
 
 static void window_unload(Window *window) {
@@ -36,6 +76,12 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
